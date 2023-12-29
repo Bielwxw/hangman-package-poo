ad00cb291d10dd5cb08075026bc9b11177df037c
1anSantos <iann.silva2006@gmail.com>
2023-12-29 10:11:45 -0300

    create tema.js

---
+++ pacote-forca/src/tema.js
@@ -0,0 +1,29 @@
+class Tema {
+  constructor(tema) {
+    this._tema = tema;
+    this._palavras = new Set();
+  }
+  getTema() {
+    return this._tema;
+  }
+  setTema(tema) {
+    this._tema = tema;
+  }
+  getPalavras() {
+    return this._palavras;
+  }
+  adicionarPalavra(palavra) {
+    const uppercase = palavra.toUpperCase();
+    this._palavras.add(uppercase);
+  }
+  removerPalavra(palavra) {
+    this._palavras.delete(palavra);
+  }
+  palavraAleatoria() {
+    const palavras = this.getPalavras();
+    const index = Math.floor(Math.random() * palavras.size);
+    return [...palavras][index];
+  }
+}
+
+module.exports = Tema;
